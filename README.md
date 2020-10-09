b1899c8698d3634642d1ce1e315f9c50dc0c02c8
asprenger <info@andresprenger.de>
2020-10-09 08:43:19 +0200

    Cleanup

---
+++ README.md
@@ -0,0 +1,9 @@
+# tea-fingerpint-64ch-model
+
+64 channel fingerprint model for tea.
+
+# Directory structure
+
+ * checkpoint: Checkpoint from the Keras classifier model
+ * src: Generated fingerprint model source
+ 
---
+++ src/tea_fingerprint_64ch_model.h
@@ -0,0 +1,7 @@
+
+#pragma once
+
+// This code is generated automatically, manuall changes will be overwritten.
+
+#include "FingerprintDescriptor.h"
+extern microinference::FingerprintDescriptor tea_fingerprint_64ch_model_descriptor;
